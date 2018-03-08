b3ee4d017d5576102f8fec191d80733244eb4f5d
Unknown <tcollin9@student.fitchburgstate.edu>
2018-03-08 17:47:50 -0500

    unity project.

---
+++ Extreme Rock-Paper-Scissors/Assets/GameController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Player playerOne, playerTwo;
+
+    private bool playerOneWins, playerTwoWins;
+
+    public static bool selectPhase, battlePhase, lvUpPhase;
+
+	// Use this for initialization
+	void Start ()
+    {
+        SetPhase(1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(selectPhase && playerOne.ready && playerTwo.ready && Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetPhase(2);
+            StartCoroutine(Battle());
+        }
+	}
+
+    IEnumerator Battle()
+    {
+        yield return null;
+    }
+
+    public void SetPhase(int phase)
+    {
+        selectPhase = false;
+        battlePhase = false;
+        lvUpPhase = false;
+        if (phase == 1)
+            selectPhase = true;
+        if (phase == 2)
+            battlePhase = true;
+        if (phase == 3)
+            lvUpPhase = true;
+    }
+
+    public void DetermineWinner()
+    {
+        playerOneWins = false;
+        playerTwoWins = false;
+
+        if (playerOne.rock && playerTwo.rock)
+        {
+            if (playerOne.tieBreaker)
+                DamagePlayer(2, playerOne.rockLV);
+            if (playerTwo.tieBreaker)
+                DamagePlayer(1, playerTwo.rockLV);
+        }
+        if(playerOne.rock && playerTwo.paper)
+        {
+            DamagePlayer(1, playerTwo.paperLV);
+        }
+        if (playerOne.rock && playerTwo.scissors)
+        {
+            DamagePlayer(2, playerOne.rockLV);
+        }
+        if (playerOne.paper && playerTwo.paper)
+        {
+            if (playerOne.tieBreaker)
+                DamagePlayer(2, playerOne.paperLV);
+            if (playerTwo.tieBreaker)
+                DamagePlayer(1, playerTwo.paperLV);
+        }
+        if (playerOne.paper && playerTwo.rock)
+        {
+            DamagePlayer(2, playerOne.paperLV);
+        }
+        if (playerOne.paper && playerTwo.scissors)
+        {
+            DamagePlayer(1, playerTwo.scissorsLV);
+        }
+        if (playerOne.scissors && playerTwo.scissors)
+        {
+            if (playerOne.tieBreaker)
+                DamagePlayer(2, playerOne.scissorsLV);
+            if (playerTwo.tieBreaker)
+                DamagePlayer(1, playerTwo.scissorsLV);
+        }
+        if (playerOne.scissors && playerTwo.rock)
+        {
+            DamagePlayer(1, playerTwo.rockLV);
+        }
+        if (playerOne.scissors && playerTwo.paper)
+        {
+            DamagePlayer(2, playerOne.scissorsLV);
+        }
+    }
+
+    public void DamagePlayer(int player, int damage)
+    {
+        if (player == 1)
+        {
+            if (playerTwo.powerAttack && !playerOne.guard)
+                damage = damage * 2;
+            if (playerOne.guard && !playerTwo.powerAttack)
+                damage = 0;
+            playerOne.hp -= damage;
+            if (damage != 0)
+                playerTwoWins = true;
+        }
+        if (player == 2)
+        {
+            if (playerOne.powerAttack && !playerTwo.guard)
+                damage = damage * 2;
+            if (playerTwo.guard && !playerOne.powerAttack)
+                damage = 0;
+            playerTwo.hp -= damage;
+            if (damage != 0)
+                playerOneWins = true;
+        }
+    }
+}
---
+++ Extreme Rock-Paper-Scissors/Assets/Player.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int player;
+
+    public SpriteRenderer rockSprite, paperSprite, scissorsSprite;
+    public Image healthBar;
+
+    [HideInInspector]
+    public bool rock, paper, scissors;
+    [HideInInspector]
+    public bool powerAttack, tieBreaker, guard;
+    public int rockLV, paperLV, scissorsLV;
+
+    public float hp;
+    public float maxHp;
+    public int abilityUses;
+
+    public bool ready;
+    public bool levelUp;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ResetChoice();
+        ResetAttack();
+
+        rockLV = 1;
+        paperLV = 1;
+        scissorsLV = 1;
+
+        maxHp = 10;
+        hp = maxHp;
+        abilityUses = 3;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameController.selectPhase && player == 1)
+            PlayerOneChoose();
+        if (GameController.selectPhase && player == 2)
+            PlayerTwoChoose();
+        SetHealthBar();
+
+    }
+
+    public void ResetChoice()
+    {
+        rock = false;
+        paper = false;
+        scissors = false;
+        ready = false;
+    }
+
+    public void ResetAttack()
+    {
+        powerAttack = false;
+        tieBreaker = false;
+        guard = false;
+
+    }
+
+    public void PlayerOneChoose()
+    {
+        //Debug.Log("hi");
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            if (!rock)
+            {
+                ResetChoice();
+                rock = true;
+                ready = true;
+                Debug.Log("rock" + rock);
+            }
+            else
+                ResetChoice();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            if (!paper)
+            {
+                ResetChoice();
+                paper = true;
+                ready = true;
+                Debug.Log("paper" + paper);
+            }
+            else
+                ResetChoice();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            if (!scissors)
+            {
+                ResetChoice();
+                scissors = true;
+                ready = true;
+                Debug.Log("scissors" + scissors);
+            }
+            else
+                ResetChoice();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (!powerAttack && abilityUses > 0)
+            {
+                ResetAttack();
+                powerAttack = true;
+                Debug.Log("power attack" + powerAttack);
+            }
+            else
+                ResetAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (!tieBreaker && abilityUses > 0)
+            {
+                ResetAttack();
+                tieBreaker = true;
+                Debug.Log("tie breaker" + tieBreaker);
+            }
+            else
+                ResetAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (!guard && abilityUses > 0)
+            {
+                ResetAttack();
+                guard = true;
+                Debug.Log("guard" + guard);
+            }
+            else
+                ResetAttack();
+        }
+    }
+
+    public void PlayerTwoChoose()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!rock)
+            {
+                ResetChoice();
+                rock = true;
+                ready = true;
+            }
+            else
+                ResetChoice();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (!paper)
+            {
+                ResetChoice();
+                paper = true;
+                ready = true;
+            }
+            else
+                ResetChoice();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (!scissors)
+            {
+                ResetChoice();
+                scissors = true;
+                ready = true;
+            }
+            else
+                ResetChoice();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (!powerAttack && abilityUses > 0)
+            {
+                ResetAttack();
+                powerAttack = true;
+            }
+            else
+                ResetAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!tieBreaker && abilityUses > 0)
+            {
+                ResetAttack();
+                tieBreaker = true;
+            }
+            else
+                ResetAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!guard && abilityUses > 0)
+            {
+                ResetAttack();
+                guard = true;
+            }
+            else
+                ResetAttack();
+        }
+    }
+
+    public void PlayerOneLvUp()
+    {
+        if (rockLV >= 10 && paperLV >= 10 && scissorsLV >= 10)
+            levelUp = false;
+        if (levelUp)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha4) && rockLV < 10)
+            {
+                rockLV++;
+                levelUp = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha7) && paperLV < 10)
+            {
+                paperLV++;
+                levelUp = false;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha8) && scissorsLV < 10)
+            {
+                scissorsLV++;
+                levelUp = false;
+            }
+        }
+    }
+
+    public void PlayerTwoLvUp()
+    {
+        if (rockLV >= 10 && paperLV >= 10 && scissorsLV >= 10)
+            levelUp = false;
+        if (levelUp)
+        {
+            if (Input.GetKeyDown(KeyCode.T) && rockLV < 10)
+            {
+                rockLV++;
+                levelUp = false;
+            }
+            if (Input.GetKeyDown(KeyCode.I) && paperLV < 10)
+            {
+                paperLV++;
+                levelUp = false;
+            }
+            if (Input.GetKeyDown(KeyCode.K) && scissorsLV < 10)
+            {
+                scissorsLV++;
+                levelUp = false;
+            }
+        }
+    }
+
+    private void SetHealthBar()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, hp/maxHp, 2 * Time.deltaTime);
+        //healthBar.fillAmount = hp / maxHp;
+    }
+
+}
